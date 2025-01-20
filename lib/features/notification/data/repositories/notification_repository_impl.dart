e20acca354a81bd6230d9a2315bdd9147cd562c9
wtfrupesh <Jadhavrupesh22@gmail.com>
2025-01-20 23:13:43 +0530

    logger added

--- lib/features/notification/data/repositories/notification_repository_impl.dart
+++ lib/features/notification/data/repositories/notification_repository_impl.dart
@@ -1,11 +1,12 @@
-import 'dart:ui';
-
 import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_notification/core/theme/app_colors.dart';
 import 'package:flutter_notification/core/constants/notification_constants.dart';
+import 'package:flutter_notification/core/theme/app_colors.dart';
+import 'package:logger/logger.dart';
+
 import '../../domain/repositories/notification_repository.dart';
+
+final logger = Logger();
 
 class NotificationRepositoryImpl implements NotificationRepository {
   final FirebaseMessaging _firebaseMessaging;
@@ -19,6 +20,7 @@ class NotificationRepositoryImpl implements NotificationRepository {
 
   @override
   Future<void> initialize() async {
+    logger.d('Initializing notification service');
     await _awesomeNotifications.initialize(
       null,
       [
@@ -41,11 +43,14 @@ class NotificationRepositoryImpl implements NotificationRepository {
 
   @override
   Future<String?> getToken() async {
-    return await _firebaseMessaging.getToken();
+    final token = await _firebaseMessaging.getToken();
+    logger.d('Firebase token: $token');
+    return token;
   }
 
   @override
   Future<void> requestPermission() async {
+    logger.d('Requesting notification permissions');
     await _firebaseMessaging.requestPermission(
       alert: true,
       badge: true,
@@ -59,23 +64,32 @@ class NotificationRepositoryImpl implements NotificationRepository {
     // Handle notification open when app is terminated
     RemoteMessage? initialMessage =
         await _firebaseMessaging.getInitialMessage();
+    logger.d('Handling initial message called');
     if (initialMessage != null) {
+      logger.d('Handling initial message: ${initialMessage.messageId}');
       onMessageHandler(initialMessage);
     }
 
     // Handle notification open when app is in background
-    FirebaseMessaging.onMessageOpenedApp.listen(onMessageHandler);
+    FirebaseMessaging.onMessageOpenedApp.listen((message) {
+      logger.d('Handling background message: ${message.messageId}');
+      onMessageHandler(message);
+    });
 
     // Handle foreground messages
     FirebaseMessaging.onMessage.listen((message) async {
+      logger.d('Handling foreground message: ${message.messageId}');
       await showNotification(message);
     });
   }
 
   @override
   Future<void> showNotification(RemoteMessage message) async {
-    await _awesomeNotifications.createNotification(
-      content: NotificationContent(
+    try {
+      logger.d('Showing notification: ${message.notification?.title}');
+      // Show notification on the device
+      await _awesomeNotifications.createNotification(
+        content: NotificationContent(
           id: message.hashCode,
           channelKey: NotificationConstants.basicChannelKey,
           title: message.notification?.title ??
@@ -87,8 +101,12 @@ class NotificationRepositoryImpl implements NotificationRepository {
           displayOnBackground: true,
           customSound: NotificationConstants.defaultSoundPath,
           wakeUpScreen: true,
-          color: AppColors.deepRed),
-    );
+          color: AppColors.deepRed,
+        ),
+      );
+    } catch (e, stackTrace) {
+      logger.e('Error showing notification', error: e, stackTrace: stackTrace);
+    }
   }
 
   @override
--- lib/features/notification/presentation/controllers/notification_controller.dart
+++ lib/features/notification/presentation/controllers/notification_controller.dart
@@ -1,35 +1,37 @@
 import 'package:awesome_notifications/awesome_notifications.dart';
-import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 
 import '../../../../core/services/navigation_service.dart';
+
+final logger = Logger();
 
 class NotificationController {
   /// Use this method to detect when a new notification or a schedule is created
   @pragma('vm:entry-point')
   static Future<void> onNotificationCreatedMethod(
       ReceivedNotification receivedNotification) async {
-    debugPrint('Notification created: ${receivedNotification.id}');
+    logger.d('Notification created: ${receivedNotification.id}');
   }
 
   /// Use this method to detect every time that a new notification is displayed
   @pragma('vm:entry-point')
   static Future<void> onNotificationDisplayedMethod(
       ReceivedNotification receivedNotification) async {
-    debugPrint('Notification displayed: ${receivedNotification.id}');
+    logger.d('Notification displayed: ${receivedNotification.id}');
   }
 
   /// Use this method to detect if the user dismissed a notification
   @pragma('vm:entry-point')
   static Future<void> onDismissActionReceivedMethod(
       ReceivedAction receivedAction) async {
-    debugPrint('Notification dismissed: ${receivedAction.id}');
+    logger.d('Notification dismissed: ${receivedAction.id}');
   }
 
   /// Use this method to detect when the user taps on a notification or action button
   @pragma('vm:entry-point')
   static Future<void> onActionReceivedMethod(
       ReceivedAction receivedAction) async {
-    debugPrint('Action received: ${receivedAction.payload}');
+    logger.d('Action received: ${receivedAction.payload}');
     if (receivedAction.payload?['deepLink'] != null) {
       final deepLink = receivedAction.payload!['deepLink']!;
       if (deepLink.contains('forget_password')) {
